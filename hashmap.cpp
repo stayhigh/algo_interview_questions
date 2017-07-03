7a065471bb0919e2a1fdf772ade923797fef6e33
stayhigh <redhero0702@gmail.com>
2017-07-03 17:56:03 +0800

    add quick item insertion syntax of hashmap

--- hashmap.cpp
+++ hashmap.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <map>
-#include <string.h>
+#include <iostream> // standard IO
+#include <map>      // map STL
+#include <string.h> // string data type
 
 /* usage: 
    c+ -std=c++11 hashmap.cpp -o hashmap && ./hashmap
@@ -20,6 +20,8 @@ int main(void){
     }
 
     //insert items into hashmap (UPDATE)
+    //insert: O(log(n))
+    //n : size of container
     ordered_map.insert(std::pair<string, string>("k1", "v1"));
     ordered_map.insert(std::pair<string, string>("k2", "v2"));
     ordered_map.insert(std::pair<string, string>("k3", "v3"));
@@ -38,6 +40,16 @@ int main(void){
     std::map<string, string>::iterator it2;
     for (it2 = ordered_map.begin(); it2 != ordered_map.end(); ++it2){
         cout << "key:"<< it2->first << "->value:"<< it2->second <<endl;
+    }
+
+    //quick items insertion for hashmap
+    // find : O(log(n))
+    std::map<string, string> qhashmap = {{"qk1", "qv1"}, {"qk2", "qv2"}};
+    auto search = qhashmap.find("qk2");
+    if (search != qhashmap.end()){
+        cout << "found "<< qhashmap["qk2"] << "->" << qhashmap["qk2"] << endl;
+    } else {
+        cout << "not found" << endl;
     }
     return 0;
 }
