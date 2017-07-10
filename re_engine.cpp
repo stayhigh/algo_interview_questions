f8b5cb9b32afb1f0781e0833b708c9f73324f4b6
stayhigh <redhero0702@gmail.com>
2017-07-10 15:30:55 +0800

    add more patterns for demo

--- re_engine.cpp
+++ re_engine.cpp
@@ -3,35 +3,75 @@
 
 using namespace std;
 
-int re_parse(const char* str, const char* pattern){
+int re_parse(char* str, char* pattern){
     int i;
     int j;
     int match = -1;
     int cnt = 0;
 
-    /* engine for question mark */
-    for (i = 0, j = 0; i < strlen(str); i++, j++){
-        cout << "i:" << i << " j:" << j <<endl; 
-            if (*(pattern + j) == '?'){
-                continue;
-            } else if (*(str + i) != *(pattern + j) ){
-                match = 0;
-                break;
-            } else if ( i == strlen(str) - 1 ){
-                match = 1;
-            }
-    }
+    char *nextchar = NULL;
+    char *nextpatternchar = NULL;
 
+    /* engine for question mark */
+    for (i = 0, j = 0; i < strlen(str); i++, j++) {
+        cout << "i:" << i << " j:" << j <<endl; 
+        if (*(pattern + j) == '?'){
+            continue;
+        } else if  (*(pattern + j) == '*') {
+            j++;
+            nextpatternchar = pattern + j;
+            while (1){
+                i++;
+                nextchar = str + i;
+                if (*nextchar == *nextpatternchar) {
+                    match = 1;
+                    break;
+                } else if (*nextchar != * nextpatternchar) {
+                    nextchar++;
+                } else if (*nextchar == '\0') {
+                    match = 0;
+                    break;
+                }
+            } 
+        } else if (*(str + i) != *(pattern + j) ) {
+            match = 0;
+            break;
+        } else if ( i == strlen(str) - 1 ) {
+            match = 1;
+        }        
+    }
     return match;
 }
 
 int main(void){
-    const char *target = "hello";
-    const char *pattern = "?e?lo";
+    // warning: ISO C++11 does not allow conversion from string literal to 'char *' [-Wwritable-strings]
+    // declare array-like strings
+    char target[] = "hello";
+    char pattern[] = "?e?lo";
+    char pattern2[] = "*llo";
+    char pattern3[] = "h*l?o";
+    char pattern4[] = "h*lo";
+
     int match = re_parse(target, pattern);
-    cout << match << endl;
     if (match) {
-        printf("pattern matched.");
+        printf("pattern matched.\n");
+    }
+
+    int match2 = re_parse(target, pattern2);
+    if (match2) {
+        printf("pattern2 matched.\n");
+    }
+
+    int match3 = re_parse(target, pattern3);
+    if (match3) {
+        printf("pattern3 matched.\n");
+    }
+
+    int match4 = re_parse(target, pattern4);
+    if (match4) {
+        printf("pattern4 matched.\n");
+    } else {
+        printf("pattern4 NOT matched.\n");
     }
 
     return 0;
